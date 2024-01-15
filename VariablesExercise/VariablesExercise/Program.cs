a7d6a3e4d8e6b705aa069e0c322acab12978c129
Isaac Oquendo <ioalvarez378@gmail.com>
2024-01-15 12:35:33 -0500

    initial commit

---
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           // Declare and initializa variables
+            string catName = "Panda";
+            int catAge = 10;
+            char middleInitial = 'O';
+            bool heIsLazy = true;
+            double weight = 13.12;
+            decimal prices = 499.99m;
+
+               // Console.WriteLine($"Cat Name: {catName}");
+               //Console.WriteLine($"Cat Age: {catAge}");
+               //Console.WriteLine($"Middle Initial: {middleInitial}");
+               //Console.WriteLine($"He Is Lazy: {heIsLazy}");
+               //Console.WriteLine($"Weight: {weight}");
+               //Console.WriteLine($"Prices: {prices}");
+
+            Console.WriteLine("Enter in your pet's name:");
+            string petsName = Console.ReadLine();
+           
+            Console.WriteLine("Enter in your pet's age:");
+            string petsAge = Console.ReadLine();
+            
+            Console.WriteLine("Enter in your pet's weight:");
+            string petsweight = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+    }
+}
+
